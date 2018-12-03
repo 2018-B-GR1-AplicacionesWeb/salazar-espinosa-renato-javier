1108b64a7dd6d9c01f07c112b0e85e8098c96fa3
RenatoJavi <36644923+RenatoJavi@users.noreply.github.com>
2018-12-03 11:14:29 -0500

    soluccion examen

---
+++ 2018-b-prueba-master/01-arreglos/Examen.js
@@ -0,0 +1,16 @@
+const arreglo = require('../data/people.json');
+console.log('HOLA MUNDO',arreglo);
+/*const  resultado = arreglo.filter(
+    function (item) { return item.gender == gender }
+);*/
+//console.log()
+function filter(data) {
+    var result = [];
+    /* result = data.filter(function (item) { return item.gender=='female'  });
+        return result;
+    }*/
+    result = data.map(resultado => resultado.gender);
+    return result;
+}
+/*var data1 = filter(arreglo);
+console.log('[ARREGLO FINAL]', data1)*/;
