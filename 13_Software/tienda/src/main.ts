cbbea3a10dd58d6cc54c3f571e76a61b2c774d16
RenatoJavi <rjavierse@hotmail.com>
2019-02-18 08:30:39 -0500

    Borrar con parametros de ruta

---
+++ 13_Software/tienda/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+    app.set('view engine','ejs');//renderizar en el main
+  await app.listen(3000);
+}
+bootstrap();
